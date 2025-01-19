4886b3864d6e26e1f4956a4eefe57a09845e8ba1
janhenriklambrechts <jan@runconverge.com>
2025-01-19 16:43:14 +0000

    add random colors to quile pattern

--- shaders/quilt.frag.glsl
+++ shaders/quilt.frag.glsl
@@ -8,26 +8,26 @@ void main() {
     vec2 coord = gl_FragCoord.xy / resolution;
 
     // Output RGB color in range from 0.0 to 1.0
-    vec3 color = vec3(coord.x, coord.y, 0.0);
+    vec3 color = vec3(coord.x, coord.y, 0.);
     color.z += abs(sin(time));
 
     // 1. Uncomment these lines to draw triangles
-    // vec2 squareCoord = 20.0 * gl_FragCoord.xy / resolution.y + vec2(time);
-    // vec2 loc = fract(squareCoord);
-    // color = vec3(smoothstep(-0.05, 0.05, loc.y - loc.x));
+    vec2 squareCoord = 20.0 * gl_FragCoord.xy / resolution.y + vec2(time);
+    vec2 loc = fract(squareCoord);
+    color = vec3(smoothstep(-0.05, 0.05, loc.y - loc.x));
 
     // 2. Uncomment these lines to invert some of the triangles
-    // vec2 cell = squareCoord - loc;
-    // if (mod(2.0 * cell.x + cell.y, 5.0) == 1.0) {
-    //     color = 1.0 - color;
-    // }
+    vec2 cell = squareCoord - loc;
+    if (mod(2.0 * cell.x + cell.y, 5.0) == 1.0) {
+        color = 1.0 - color;
+    }
 
     // 3. Uncomment these lines to produce interesting colors
-    // float c = mod(3.0 * cell.x + 2.0 * cell.y, 7.0) / 7.0;
-    // color = 1.0 - (1.0 - color) * vec3(c, c, c);
+    vec3 c = vec3(mod(3.0 * cell.x + 2.0 * cell.y, 7.0) / 7.0, mod(2.0 * cell.x + 2.0 * cell.y, 5.0) / 5.0, mod(4.0 * cell.x + 2.0 * cell.y, 4.0) / 4.0);
+    color = 1.0 - (1.0 - color) * c;
 
     // 4. Uncomment to lighten the colors
-    // color = sqrt(color);
+    color = sqrt(color);
 
     gl_FragColor = vec4(color, 1.0);
 }
